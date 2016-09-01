bba9778a0eb9a969dd91e3c7227cabad4a75e570
Dylan Lynch <lynchd2@tcnj.edu>
2016-09-01 11:22:29 -0400

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  get 'customers/index'
+
+  devise_for :users
+  get 'dashboard/index'
+
+  root "dashboard#index"
+
+  resources :customers, only: [:index]
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
